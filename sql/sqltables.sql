4ba29c2bbb9d54caa36dc0c7a8bb530627b8398f
MaritaIfemeni <marita.ifemeni@student.hamk.fi>
2022-11-28 10:49:01 +0200

    sql

---
+++ sql/sqltables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Areas
+(
+   ID int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   parking_area varchar(50) NOT NULL,
+   address varchar(200) NOT NULL,
+);
+
+insert into Areas(parking_area, address)
+values('Kauniasten terveyskeskus','Asematie 19, 02700 Kauniainen'),
+('Suu- ja hammaskeskus','Haartmaninkatu 1 A 00290 Helsinki');
+
+SELECT parking_area, address FROM Areas;
+
+SELECT * FROM Areas;
