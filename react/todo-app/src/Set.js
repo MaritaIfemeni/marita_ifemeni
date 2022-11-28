5f4f901f3756949380666bc5c96c562e54de7da2
MaritaIfemeni <marita.ifemeni@student.hamk.fi>
2022-11-28 13:42:28 +0200

    update

--- react/todo-app/src/Set.js
+++ react/todo-app/src/Set.js
@@ -2,43 +2,49 @@ import React from "react";
 import { useState } from "react";
 
 const Set = () => {
-    const [todos, setTodos] = useState(0);
-    const add = () => {
-        setTodos((c) => c + 6);}
-    
-        const [message, setMessage] = useState('');
-        const handleChange = event => {
-        setMessage(event.target.value);
-        console.log('value is:', event.target.value);}
-    
-    return (
-        <>
-         <div>
-      <input
-        type="text"
-        id="message"
-        name="message"
-        onChange={handleChange}
-        value={message}
-      />
+  const [todos, setTodos] = useState("");
+  const add = () => {
+    setTodos((c) => listOfFreeSpots);
+  };
 
-      <h2>Tasks: {message}</h2>
-      <button onClick={handleChange}>Log message</button>
-    </div>
-        <form>
-            <label>Enter your task that needs to be complited:
-            < input type="text" />
-            </label>
-        </form>
+  const [message, setMessage] = useState("");
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+    console.log("value is:", event.target.value);
+  };
+  const listOfFreeSpots = [
+    "Aurinkokiven koulu",
+    "Keravan asema",
+    "Jumbon Parkkialue",
+    "Pielaveden K-Marketti",
+  ];
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          id="message"
+          name="message"
+          onChange={handleChange}
+          value={message}
+        />
 
-        <div>
-          Todoes: {todos}
-          <button onClick={add}>Add Task To Do</button>
-        </div>
-      </>
-      );
-    
+        <h2>Tasks: {message}</h2>
+        <button onClick={handleChange}>Log message</button>
+      </div>
+      <form>
+        <label>
+          Enter your task that needs to be complited:
+          <input type="text" />
+        </label>
+      </form>
+
+      <div>
+        Todoes: {todos}
+        <button onClick={add}>Add Task To Do</button>
+      </div>
+    </>
+  );
 };
-
 
 export default Set;
