83d45e3e91d45187fa17b84d7869ddee62ffb549
MaritaIfemeni <marita.ifemeni@student.hamk.fi>
2022-12-19 12:20:00 +0200

    5.assignment

--- js/helloreact/src/App.js
+++ js/helloreact/src/App.js
@@ -7,7 +7,10 @@ function App() {
 
   return (
     <div className="App">
-      You have presset the button {count} times <br />
+      {count <= 10
+      ? <p>You have pressed the button {count} times</p>
+      : <p>You have pressed the butto more than 10 times</p>
+      }
       <button onClick={() => setCount(count + 1)}>Press me</button>
     </div>
   );
--- js/helloreact/src/index.js
+++ js/helloreact/src/index.js
@@ -8,8 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App/>
-    <App />
-    <App />
   </React.StrictMode>
 );
 
