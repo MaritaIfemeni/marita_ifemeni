e79233f8bdd82e7a5bdbce6845bdf493a70ca129
MaritaIfemeni <marita.ifemeni@student.hamk.fi>
2022-12-28 12:26:47 +0200

    todo assignment part 1

---
+++ js/todolist/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todo, setTodo] = useState({ description: "", date: "" });
+
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  };
+
+  const addTodo = () => {
+    setTodos([...todos, todo]);
+    setTodo({description: '', date: ''}); // Clear todo
+  };
+
+  return (
+    <div className="App">
+      <input
+        placeholder="Description"
+        name="description"
+        value={todo.description}
+        onChange={inputChanged}
+      />
+      <input
+        placeholder="Date"
+        name="date"
+        value={todo.date}
+        onChange={inputChanged}
+      />
+      <button onClick={addTodo}>Add</button>
+      <table>
+        <tbody>
+          {todos.map((todo, index) => (
+            <tr key={index}>
+              <td>{todo.description}</td>
+              <td>{todo.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
