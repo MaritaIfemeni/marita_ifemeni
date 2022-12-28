d2b6c56e474ef34edd0ce5d4b2fd6ce32526a763
MaritaIfemeni <marita.ifemeni@student.hamk.fi>
2022-12-28 11:42:33 +0200

    form

--- js/helloreact/src/Multipleuserinput.js
+++ js/helloreact/src/Multipleuserinput.js
@@ -14,30 +14,38 @@ function Multipleuserinput() {
   const inputChanged = (event) => {
     setPerson({ ...person, [event.target.name]: event.target.value });
   };
-  
+
+  const formSubmitted = (event) => {
+    event.preventDefault();
+    showAlert();
+    // Do somethin with you data
+  };
+
   return (
     <div>
       Name: {person.firstname} {person.lastname} Email: {person.email}
       <br />
-      <input
-        placeholder="First name"
-        name="firstname"
-        value={person.firstname}
-        onChange={inputChanged}
-      />
-      <input
-        placeholder="Last name"
-        name="lastname"
-        value={person.lastname}
-        onChange={inputChanged}
-      />
-      <input
-        placeholder="Email"
-        name="email"
-        value={person.email}
-        onChange={inputChanged}
-      />
-      <button onClick={showAlert}>Submit</button>
+      <form onSubmit={formSubmitted}>
+        <input
+          placeholder="First name"
+          name="firstname"
+          value={person.firstname}
+          onChange={inputChanged}
+        />
+        <input
+          placeholder="Last name"
+          name="lastname"
+          value={person.lastname}
+          onChange={inputChanged}
+        />
+        <input
+          placeholder="Email"
+          name="email"
+          value={person.email}
+          onChange={inputChanged}
+        />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
