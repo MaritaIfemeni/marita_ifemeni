61bc8c5167d035ac4ec9a31e98d3fb4e834c55cf
MaritaIfemeni <marita.ifemeni@student.hamk.fi>
2023-01-07 21:45:53 +0200

    mui prac

--- js/helloreact/src/Router.js
+++ js/helloreact/src/Router.js
@@ -1,21 +1,30 @@
 import React from "react";
-import { BrowserRouter, Routes, Route,Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Todolist from "./components/Todolist";
 import NotFound from "./components/Notfound";
+import "./App.css";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 function Router() {
   return (
     <div>
       <BrowserRouter>
-        <nav>
-          <Link to="/">Home</Link>{" "}
-          <Link to="/About">About</Link>{" "}
-          <Link to="/Contact">Contact</Link>{" "}
-          <Link to="/Todolist">To Do App</Link>{" "}
-        </nav>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6">
+              <nav>
+                <Link to="/">Home</Link> <Link to="/About">About</Link>{" "}
+                <Link to="/Contact">Contact</Link>{" "}
+                <Link to="/Todolist">To Do App</Link>{" "}
+              </nav>
+            </Typography>
+          </Toolbar>
+        </AppBar>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
--- js/helloreact/src/components/Todolist.js
+++ js/helloreact/src/components/Todolist.js
@@ -1,37 +1,79 @@
-import React, { useState } from 'react';
-import '../App.css';
+import React, { useState } from "react";
+import "../App.css";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 function Todolist() {
-  const [todo, setTodo] = useState({description: '', date: ''});
+  const [todo, setTodo] = useState({ description: "", date: "" });
   const [todos, setTodos] = useState([]);
 
-  const inputChanged = (event) => {
-    setTodo({...todo, [event.target.name]: event.target.value});
-  }
+  const TextFieldChanged = (event) => {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  };
 
   const addTodo = () => {
     setTodos([...todos, todo]);
-  }
+  };
 
   const deleteTodo = (row) => {
     setTodos(todos.filter((todo, index) => index !== row));
-  }
+  };
 
   return (
     <div className="App">
-      <input placeholder="Description" name="description" value={todo.description} onChange={inputChanged} />
-      <input placeholder="Date" name="date" value={todo.date} onChange={inputChanged}/>
-      <button onClick={addTodo}>Add</button>
+      <AppBar className="todo" position="static">
+        <Toolbar>
+          <Typography variant="h6">My Todolist</Typography>
+        </Toolbar>
+      </AppBar>
+      <Stack
+        direction="row"
+        spacing={2}
+        mt={2}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <TextField
+          variant="standard"
+          label="Description"
+          name="description"
+          value={todo.description}
+          onChange={TextFieldChanged}
+        />
+        <TextField
+          variant="standard"
+          label="Date"
+          name="date"
+          value={todo.date}
+          onChange={TextFieldChanged}
+        />
+        <Button variant="outlined" onClick={addTodo}>
+          Add
+        </Button>
+      </Stack>
       <table>
         <tbody>
-       {
-          todos.map((todo, index) => 
+          {todos.map((todo, index) => (
             <tr key={index}>
               <td>{todo.description}</td>
-             <td>{todo.date}</td>
-             <td><button onClick={() => deleteTodo(index)}>Delete</button></td>
-            </tr>)
-        }
+              <td>{todo.date}</td>
+              <td>
+                <IconButton
+                  size="small"
+                  color="error"
+                  onClick={() => deleteTodo(index)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
