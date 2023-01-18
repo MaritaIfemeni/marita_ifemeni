17ee7070d5f7a66b505f7a1e3126ea199dc45f23
MaritaIfemeni <marita.ifemeni@student.hamk.fi>
2023-01-18 19:16:22 +0200

    test prac 2

--- js/todolist/src/App.test.js
+++ js/todolist/src/App.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TodoTable from "./components/TodoTable.js";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 import "@testing-library/jest-dom/extend-expect";
 
 test("renders todotable", () => {
@@ -8,4 +9,19 @@ test("renders todotable", () => {
   render(<TodoTable todos={row} />);
     const tableRow = screen.getByText('Go to coffee');
   expect(tableRow).toBeInTheDocument();
+})
+
+test('add todo',() => {
+  render(<App/>);
+  fireEvent.click(screen.getByText('To Do List'))
+
+  const desc = screen.getByPlaceholderText('Description');
+  fireEvent.change(desc, { target: { value: 'Go to coffee' } })
+  const date = screen.getByPlaceholderText('Date');
+  fireEvent.change(date, { target: { value: '29.11.2022' } })
+
+  fireEvent.click(screen.getByText('Add'));
+  const tableRow = screen.getByText('Go to coffee');
+  expect(tableRow).toBeInTheDocument();
+  
 });
