f7303e67f10efcaea7ad7f61fa77ec44a780baf3
MaritaIfemeni <marita.ifemeni@student.hamk.fi>
2023-01-18 18:26:36 +0200

    test prac one

--- js/todolist/src/App.test.js
+++ js/todolist/src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import TodoTable from "./components/TodoTable.js";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders todotable", () => {
+  const row = [{ description: "Go to coffee", date: "24.12.2023" }]
+  render(<TodoTable todos={row} />);
+    const tableRow = screen.getByText('Go to coffee');
+  expect(tableRow).toBeInTheDocument();
 });
