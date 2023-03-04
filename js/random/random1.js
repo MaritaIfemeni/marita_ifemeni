e054554f81daabafee1fb49c4face417865f3576
MaritaIfemeni <marita.ifemeni@student.hamk.fi>
2023-03-04 08:51:13 +0100

    loops

---
+++ js/random/random1.js
@@ -0,0 +1,33 @@
+
+
+function solution(number) {
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+const test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let x = 0; x < 5; x++) {
+  console.log(test[x]);
+}
+
+for (let y = 0; y < 5; y++) {
+  if (test[y] > 5) {
+  }
+  console.log("tässä" + test[y] * 2);
+}
+
+
+
+let even = [];
+for (let z = 0; z < 5; z++) {
+    if (test[z] % 2 === 0) {
+        even.push(test[z]);
+    }
+    }
+console.log(even);
+
